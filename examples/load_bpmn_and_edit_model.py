f6b8202fbce1222299f1026a1f4652d698b832ac
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-07-18 13:27:05 +0200

    added load_bpmn_and_edit_model example

---
+++ examples/load_bpmn_and_edit_model.py
@@ -0,0 +1,18 @@
+import pm4py
+from utils.llm_model_generator import LLMProcessModelGenerator
+from utils.general_utils import pt_to_powl_code
+
+
+def execute_script():
+    bpmn_graph = pm4py.read_bpmn("running-example.bpmn")
+    # works for BPMNs that are block-structured in the control-flow
+    process_tree = pm4py.convert_to_process_tree(bpmn_graph)
+    powl_code = pt_to_powl_code.recursively_transform_process_tree(process_tree)
+    obj = LLMProcessModelGenerator(None, "sk-",
+                                   powl_model_code=powl_code, openai_model="gpt-4o")
+    obj.update("Can you add an activity Throw Chair in the end")
+    obj.view_bpmn("svg")
+
+
+if __name__ == "__main__":
+    execute_script()
