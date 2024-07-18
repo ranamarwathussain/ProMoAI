38136eb95363a9cbc2dcdc9198ca2ff0422cc0df
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-07-18 13:22:10 +0200

    added example discover_and_edit_model

---
+++ examples/discover_and_edit_model.py
@@ -0,0 +1,17 @@
+import pm4py
+from utils.llm_model_generator import LLMProcessModelGenerator
+from utils.general_utils import pt_to_powl_code
+
+
+def execute_script():
+    log = pm4py.read_xes("running-example.xes")
+    process_tree = pm4py.discover_process_tree_inductive(log)
+    powl_code = pt_to_powl_code.recursively_transform_process_tree(process_tree)
+    obj = LLMProcessModelGenerator(None, "sk-",
+                                   powl_model_code=powl_code, openai_model="gpt-4o")
+    obj.update("Can you add an activity Explode Bomb in the end")
+    obj.view_bpmn("svg")
+
+
+if __name__ == "__main__":
+    execute_script()
