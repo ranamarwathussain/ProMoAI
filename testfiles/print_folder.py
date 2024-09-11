0fe9bcbb88996fbac3dcfef759b5fdb54e730b2d
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-09-11 15:27:10 +0200

    added print_folder.py script

---
+++ testfiles/print_folder.py
@@ -0,0 +1,10 @@
+import os
+
+FOLDER = "long"
+
+for proc in os.listdir(FOLDER):
+    proc_path = os.path.join(FOLDER, proc)
+
+    proc_content = open(proc_path, "r").read().strip().replace("\n", " ").replace("\r", " ").strip()
+
+    print(proc_path.replace("\\", "/"), "\t", proc_content)
