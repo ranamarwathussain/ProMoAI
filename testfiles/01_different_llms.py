e3e3bf3fcda2e9b5e7be5b9b5731e06bbaf52c69
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-09-11 11:30:34 +0200

    updated file

--- testfiles/01_different_llms.py
+++ testfiles/01_different_llms.py
@@ -2,24 +2,36 @@ import os
 import pm4py
 import json
 from utils import llm_model_generator, shared
+from utils.general_utils import improve_descr
 
 
 api_url = open("api_url.txt", "r").read().strip()
 api_key = open("api_key.txt", "r").read().strip()
 openai_model = open("api_model.txt", "r").read().strip()
 n_candidates = 1
+improve_initial_prompt = "0"
+improve_resulting_model = "0"
+
 
 folders = ["short", "medium", "long"]
 
 for fold in folders:
     for proc_file in os.listdir(fold):
-        output_file = os.path.join("results", "01_diff_llms__"+openai_model.replace("/", "").replace(":", "")+"__"+fold+"__"+proc_file)
+        output_file = os.path.join("results", openai_model.replace("/", "").replace(":", "")+"__"+fold+"__"+str(n_candidates)+"__"+improve_initial_prompt+"__"+improve_resulting_model+"__"+proc_file)
         proc_descr = open(os.path.join(fold, proc_file), "r").read().strip()
 
         if not os.path.exists(output_file):
             try:
+                if improve_initial_prompt == "1":
+                    proc_descr = improve_descr.improve_process_description(proc_descr, api_key, openai_model, api_url=api_url)
+
                 obj = llm_model_generator.initialize(process_description=proc_descr, api_key=api_key, openai_model=openai_model,
-                                                     api_url=api_url, n_candidates=n_candidates)
+                                                     api_url=api_url, n_candidates=n_candidates, debug=False)
+
+                if improve_resulting_model == "1":
+                    feedback = "Please improve the process model"
+
+                    obj = llm_model_generator.update(obj, feedback, n_candidates=n_candidates, debug=False)
 
                 powl = obj.process_model
                 net, im, fm = pm4py.convert_to_petri_net(powl)
