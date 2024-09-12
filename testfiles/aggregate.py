de397d778de660c436c6ee0ec121f51eb1ee14b9
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-09-12 07:24:47 +0200

    modified script

--- testfiles/aggregate.py
+++ testfiles/aggregate.py
@@ -7,7 +7,7 @@ api_url = open("api_url.txt", "r").read().strip()
 api_key = open("api_key.txt", "r").read().strip()
 openai_model = open("api_model.txt", "r").read().strip()
 n_candidates = 1
-improve_initial_prompt = "1"
+improve_initial_prompt = "0"
 improve_resulting_model = "0"
 
 agg_cat = {}
@@ -25,16 +25,20 @@ for file in os.listdir("results"):
         if ite == str(n_candidates) and pi == improve_initial_prompt and mi == improve_resulting_model:
             contents = json.load(open(os.path.join("results", file), "r"))
             if cat not in agg_cat:
-                agg_cat[cat] = {"visible_transitions": [], "self_grading": []}
+                agg_cat[cat] = {"visible_transitions": [], "self_grading": [], "n_iterations": []}
 
             agg_cat[cat]["visible_transitions"].append(contents["visible_transitions"])
             agg_cat[cat]["self_grading"].append(contents["self_grading"])
+            agg_cat[cat]["n_iterations"].append(contents["n_iterations"])
+
 
 for cat in agg_cat:
     agg_cat[cat]["visible_transitions"] = (float(np.mean(agg_cat[cat]["visible_transitions"])), float(np.std(agg_cat[cat]["visible_transitions"])))
     agg_cat[cat]["self_grading"] = (float(np.mean(agg_cat[cat]["self_grading"])), float(np.std(agg_cat[cat]["self_grading"])))
+    agg_cat[cat]["n_iterations"] = (float(np.mean(agg_cat[cat]["n_iterations"])), float(np.std(agg_cat[cat]["n_iterations"])))
 
     agg_cat[cat]["visible_transitions"] = "%.1f pm %.1f" % (agg_cat[cat]["visible_transitions"][0], agg_cat[cat]["visible_transitions"][1])
     agg_cat[cat]["self_grading"] = "%.1f pm %.1f" % (agg_cat[cat]["self_grading"][0], agg_cat[cat]["self_grading"][1])
+    agg_cat[cat]["n_iterations"] = "%.1f pm %.1f" % (agg_cat[cat]["n_iterations"][0], agg_cat[cat]["n_iterations"][1])
 
-    print(cat, "visible_transitions", agg_cat[cat]["visible_transitions"], "\tself_grading", agg_cat[cat]["self_grading"])
+    print(cat, "visible_transitions", agg_cat[cat]["visible_transitions"], "\tself_grading", agg_cat[cat]["self_grading"], "\tn_iterations", agg_cat[cat]["n_iterations"])
