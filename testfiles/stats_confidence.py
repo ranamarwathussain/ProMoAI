6a3c27adc2143f40c4b00b655e558f6ae1e299c7
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-09-12 11:05:54 +0200

    stats_confidence.py test file

---
+++ testfiles/stats_confidence.py
@@ -0,0 +1,28 @@
+import os
+import json
+import numpy as np
+
+TARGET_MODEL = "gpt-4o-mini"
+X_CATEGORY = "visible_transitions"
+
+rrr = {}
+
+for file in os.listdir("results"):
+    if file.startswith("gpt-4o-mini"):
+        contents = json.load(open(os.path.join("results", file), "r"))
+
+        grading = contents["self_grading"]
+        x_value = contents[X_CATEGORY]
+
+        if x_value not in rrr:
+            rrr[x_value] = []
+
+        rrr[x_value].append(grading)
+
+for x_value in rrr:
+    rrr[x_value] = float(np.mean(rrr[x_value]))
+
+rrr = [(x, y) for x, y in rrr.items()]
+rrr.sort(key=lambda z: (z[0], z[1]))
+
+print(rrr)
